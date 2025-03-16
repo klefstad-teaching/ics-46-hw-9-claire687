b3642abcf53644e0b26d3c5e8dcaadd953c48523
claire687 <junyil32@uci.edu>
2025-03-16 08:31:47 +0000

    fixed the print in dikstras

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -51,6 +51,6 @@ void print_path(const vector<int>& v, int total) {
     for (size_t i = 0; i < v.size(); i++) {
         cout << v[i] << " ";
     }
-    cout << total << endl;
+    cout << endl << "Total cost is "<< total << endl;
 }
 
